aab82bee70c146dc3767b62ae5f7d202958455ce
Slimane Achbouq <slimane321@gmail.com>
2023-06-19 00:53:52 +0200

    added required functionalities & update tests

--- src/classes/game.js
+++ src/classes/game.js
@@ -1,21 +1,188 @@
 "use strict";
 
-import { createMadredeDiosMap } from '../helpers/gameHelper.js';
+import { createMadredeDiosMap, cellTypes, orientations } from '../helpers/gameHelper.js';
+import Mountain from './mountain.js';
+import Treasure from './treasure.js';
+import Adventurer from './adventurer.js';
 
 export default class Game {
     constructor(width, height, fileLines) {
-        this._madredeDiosMap = createMadredeDiosMap(+width, +height, 'P');
+        this._madredeDiosMap = createMadredeDiosMap(+width, +height, cellTypes.PLAIN);
         this._gameLength = 0;
         this._mountains = [];
         this._treasures = [];
         this._adventurers = [];
+    
+        this._initializeMap(fileLines);
+    }
+
+    _initializeMap(fileLines) {
+        fileLines.forEach(line => {
+            const lineType = line[0];
+
+            switch (lineType) {
+                case cellTypes.MOUNTAIN:
+                    this.addMountain(line);
+                    break;
+                case cellTypes.TREASURE:
+                    this.addTreasure(line);
+                    break;
+                case cellTypes.ADVENTURER:
+                    this.addAdventurer(line);
+                    break
+            }
+        });
+    }
+
+    addMountain(line) {
+        const [_, xAxis, yAxis] = line.split(' - ');
+        const mountain = new Mountain(+xAxis, +yAxis);
+    
+        this.setMapPositionCellType(mountain, cellTypes.MOUNTAIN);
+        this._mountains.push(mountain);
+    }
+
+    addTreasure(line) {
+        const [_, xAxis, yAxis, nbrTreasures] = line.split(' - ');
+        const treasure = new Treasure(+xAxis, +yAxis, +nbrTreasures);
+    
+        this.setMapPositionCellTreasure(treasure);
+        this._treasures.push(treasure);
+    }
+
+    addAdventurer(line) {
+        const [_, adventurerName, xAxis, yAxis, orientation, movements] = line.split(' - ');
+        const adventurer = new Adventurer(adventurerName, +xAxis, +yAxis, orientation, movements);
+    
+        this.setMapPositionCellAdventurer(adventurer);
+        
+        if (adventurer.movements.length > this._gameLength) {
+          this._gameLength = adventurer.movements.length;
+        }
+        this._adventurers.push(adventurer);
+    }
+
+    setMapPositionCellType(position, cellType) {
+        const { xAxis, yAxis } = position;
+        this._madredeDiosMap[yAxis][xAxis].type = cellType;
+    }
+
+    setMapPositionCellTreasure(treasure) {
+        const { xAxis, yAxis } = treasure;
+        this._madredeDiosMap[yAxis][xAxis].treasure = treasure;
+    }
+
+    setMapPositionCellAdventurer(adventurer) {
+        const { xAxis, yAxis } = adventurer;
+        if (this._madredeDiosMap[yAxis][xAxis].hasAdventurer === true) {
+            throw new Error('Adventurers can not start from the same position !');
+        }
+        this._madredeDiosMap[yAxis][xAxis].hasAdventurer = true;
+    }
+
+    startExploringMap() {
+        for (let round = 0; round < this._gameLength; round++) {
+          this._adventurers = this.playRound(round);
+        }
+    }
+
+    playRound(round) {
+        return this._adventurers.map(adventurer => {
+          const { xAxis: previousX, yAxis: previousY } = adventurer;
+          const adventurerAfterMove = this.makeMove(adventurer, adventurer.movements[round]);
+          const { xAxis: newX, yAxis: newY } = adventurerAfterMove;
+    
+          const newCell = this._madredeDiosMap[newY][newX];
+          if (newCell.treasure && newCell.treasure.nbrTreasures && (newX !== previousX || newY !== previousY)) {
+            adventurerAfterMove.nbrTreasures++;
+            newCell.treasure.nbrTreasures--;
+          }
+
+          return adventurerAfterMove;
+        });
+    }
+
+    makeMove(adventurer, moveType) {
+        if (moveType === 'A') {
+          const { xAxis, yAxis } = this.getNextPosition(adventurer);
+          return this.isAbleToMove(xAxis, yAxis) ? { ...adventurer, xAxis, yAxis } : adventurer;
+        }
+    
+        const { orientation } = adventurer;
+        const newOrientation = this.getNextOrientation(orientation, moveType);
+        return { ...adventurer, orientation: newOrientation };
+    }
+
+    getNextPosition(adventurer) {
+      const { orientation, xAxis, yAxis } = adventurer;
+    
+      const positionMap = {
+        [orientations.NORTH]: { xAxis, yAxis: yAxis - 1 > 0 ? yAxis - 1 : 0 },
+        [orientations.SOUTH]: { xAxis, yAxis: yAxis + 1 >= this.height ? yAxis : yAxis + 1 },
+        [orientations.EAST]: { yAxis, xAxis: xAxis + 1 >= this.width ? xAxis : xAxis + 1 },
+        [orientations.WEST]: { yAxis, xAxis: xAxis - 1 > 0 ? xAxis - 1 : 0 },
+      };
+    
+      return positionMap[orientation];
+    }
+
+    getNextOrientation(orientation, moveType) {
+      const orientationMap = {
+        [orientations.NORTH]: {
+          'A': orientations.NORTH,
+          'D': orientations.EAST,
+          'G': orientations.WEST
+        },
+        [orientations.SOUTH]: {
+          'A': orientations.SOUTH,
+          'D': orientations.WEST,
+          'G': orientations.EAST
+        },
+        [orientations.EAST]: {
+          'A': orientations.EAST,
+          'D': orientations.SOUTH,
+          'G': orientations.NORTH
+        },
+        [orientations.WEST]: {
+          'A': orientations.WEST,
+          'D': orientations.NORTH,
+          'G': orientations.SOUTH
+        }
+      };
+    
+      return orientationMap[orientation][moveType];
+    }
+    
+    isAbleToMove(xAxis, yAxis) {
+        const cell = this._madredeDiosMap[yAxis][xAxis];
+        return !(cell.type === cellTypes.MOUNTAIN || cell.hasAdventurer);
+    }
+
+    get gameLength() {
+        return this._gameLength;
+    }
+    
+    get adventurers() {
+        return this._adventurers;
+    }
+    
+    get mountains() {
+        return this._mountains;
+    }
+    
+    get treasures() {
+        return this._treasures;
+    }
+    
+    get madredeDiosMap() {
+      return this._madredeDiosMap;
     }
 
     get width() {
-        return this._madredeDiosMap[0].length;
-      }
-  
-      get height() {
-        return this._madredeDiosMap.length;
-      }
+      return this._madredeDiosMap[0].length;
+    }
+
+    get height() {
+      return this._madredeDiosMap.length;
+    }
 }
--- src/tests/testGame.js
+++ src/tests/testGame.js
@@ -1,23 +1,121 @@
-"use strict";
-
-import { readInputFile } from '../helpers/fileIOHelper.js';
+import { readFileSync, existsSync, unlinkSync } from 'fs';
 import { expect } from 'chai';
+import { readInputFile, writeOutputFile } from '../helpers/fileIOHelper.js';
 import Game from '../classes/game.js';
+import Treasure from '../classes/treasure.js';
+import Adventurer from '../classes/adventurer.js';
+import { OUTPUT_FILE_PATH } from '../../config.js';
 
 describe('Game', () => {
-    describe('Parsing files', () => {
-        it('Should read input file correctly', () => {
-          const fileLines = readInputFile();
-          const lineToInitMap = fileLines.find(line => line.startsWith('C'));
-          const [ _, mapWidth, mapHeight] = lineToInitMap.split('-');
-          expect(lineToInitMap).to.equal('C - 3 - 4');
-          expect(+mapWidth).to.equal(3);
-          expect(+mapHeight).to.equal(4);
+  let game;
+  let mockFileLines = [];
 
-          const game = new Game(+mapWidth, +mapHeight, fileLines);
-          expect(game.width).to.equal(+mapWidth);
-          expect(game.height).to.equal(+mapHeight);
-        });
+  describe('Parsing files', () => {
+    it('should read input file correctly', () => {
+      const fileLines = readInputFile();
+      const lineToInitMap = fileLines.find(line => line.startsWith('C'));
+      const [ _, mapWidth, mapHeight] = lineToInitMap.split('-');
+      expect(lineToInitMap).to.equal('C - 3 - 4');
+      expect(+mapWidth).to.equal(3);
+      expect(+mapHeight).to.equal(4);
+
+      mockFileLines = fileLines;
+    });
+
+    it('should write output file', () => {
+      expect(existsSync(OUTPUT_FILE_PATH)).to.be.false;
+      writeOutputFile(game);
+      expect(existsSync(OUTPUT_FILE_PATH)).to.be.true;
+      
+      const outputFileLines = readFileSync(OUTPUT_FILE_PATH, 'utf8').split('\n');
+      const mountains = outputFileLines.filter(line => line.startsWith('M'));
+      expect(mountains.length).to.be.equal(2);
+      expect(mountains[0]).to.be.equal('M - 1 - 0');
+      expect(mountains[1]).to.be.equal('M - 2 - 1');
+      expect(outputFileLines.find(line => line.startsWith('C'))).to.equal('C - 3 - 4');
+      expect(outputFileLines.find(line => line.startsWith('A'))).to.equal('A - Lara - 1 - 1 - S - 0');
+    });
+
+    after(() => {
+      unlinkSync(OUTPUT_FILE_PATH);
+    });
+  });
+
+  beforeEach(() => {
+    game = new Game(3, 4, mockFileLines);
+  });
+
+  describe('Initialization', () => {
+    it('should create the game with the specified width and height', () => {
+      expect(game.madredeDiosMap[0].length).to.equal(3);
+      expect(game.madredeDiosMap.length).to.equal(4);
+    });
+
+    it('should add mountains to the game', () => {
+      expect(game.mountains).to.have.lengthOf(2);
+      expect(game.mountains[0]).to.deep.equal({ xAxis: 1, yAxis: 0 });
+      expect(game.mountains[1]).to.deep.equal({ xAxis: 2, yAxis: 1 });
+    });
+
+    it('should add treasures to the game', () => {
+      expect(game.treasures).to.have.lengthOf(2);
+      expect(game.treasures[0]).to.deep.equal({ xAxis: 0, yAxis: 3, nbrTreasures: 2 });
+      expect(game.treasures[1]).to.deep.equal({ xAxis: 1, yAxis: 3, nbrTreasures: 3 });
+    });
+
+    it('should add adventurers to the game', () => {
+      expect(game.adventurers).to.have.lengthOf(1);
+      expect(game.adventurers[0]).to.deep.equal({
+        adventurerName: 'Lara',
+        xAxis: 1,
+        yAxis: 1,
+        orientation: 'S',
+        movements: ['A', 'A', 'D', 'A', 'D', 'A', 'G', 'G', 'A'],
+        nbrTreasures: 0
       });
+    });
+  });
+
+  describe('Map Operations', () => {
+    it('should set map position cell type correctly', () => {
+      expect(game._madredeDiosMap[2][1].type).to.equal('P');
+      game.setMapPositionCellType({ xAxis: 1, yAxis: 2 }, 'M');
+      expect(game._madredeDiosMap[2][1].type).to.equal('M');
+    });
+
+    it('should set map position cell treasure correctly', () => {
+      const treasure = new Treasure(1, 2, 2);
+      game.setMapPositionCellTreasure(treasure);
+      expect(game._madredeDiosMap[2][1].treasure).to.deep.equal(treasure);
+    });
+
+    it('should set map position cell adventurer correctly', () => {
+      expect(game._madredeDiosMap[2][1].hasAdventurer).to.be.false;
+      const adventurer = new Adventurer('Slimane', 2, 2, 'S', 'AADADAGGA');
+      game.setMapPositionCellAdventurer(adventurer);
+      expect(game._madredeDiosMap[1][1].hasAdventurer).to.be.true;
+    });
+  });
+
+  describe('Game play', () => {
+    it('should make a move for the adventurer', () => {
+      const adventurer = game.adventurers[0];
+      const newAdventurer = game.makeMove(adventurer, 'A');
+
+      expect(newAdventurer.xAxis).to.equal(1);
+      expect(newAdventurer.yAxis).to.equal(2);
+    });
+
+    it('should update the game state after a round of play', () => {
+      game.startExploringMap();
+      const adventurer = game.adventurers[0];
+
+      expect(adventurer.xAxis).to.equal(0);
+      expect(adventurer.yAxis).to.equal(3);
+      expect(adventurer.nbrTreasures).to.equal(3);
+      expect(adventurer.orientation).to.equal('S');
+      expect(game._treasures[0].nbrTreasures).to.equal(0);
+      expect(game._treasures[1].nbrTreasures).to.equal(2);
+    });
+  });
 });
-  
